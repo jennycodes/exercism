998ea7322deaa771d4e122eb7523bb62aad184a3
jbunny13 <jnguyen72086@gmail.com>
2016-04-09 10:28:42 -0600

    New iteration of rna transcription

--- ruby/rna-transcription/rna_transcription.rb
+++ ruby/rna-transcription/rna_transcription.rb
@@ -1,10 +1,17 @@
 class Complement
   VERSION = 3
   def self.of_dna(strand)
+    dna_rna_complements = {
+      'G' => 'C',
+      'C' => 'G',
+      'T' => 'A',
+      'A' => 'U'
+    }
+
     if strand =~ /[^GCTA]/
       raise ArgumentError.new("Invalid strand!")
     else
-      strand.gsub(/[GCTA]/, 'G' => 'C', 'C' => 'G', 'T' => 'A', 'A' => 'U')
+      strand.gsub(/[GCTA]/, dna_rna_complements)
     end
   end
 end
