b5a8bab69a776e0353348000ab8d721053fe9cf0
jbunny13 <jnguyen72086@gmail.com>
2016-04-12 19:54:15 -0600

    New iteration of gigasecond

--- ruby/gigasecond/gigasecond.rb
+++ ruby/gigasecond/gigasecond.rb
@@ -1,6 +1,8 @@
 class Gigasecond
   VERSION = 1
   def self.from(start_date)
+    msg = 'Must be entered in (Time.utc(1986, 7, 20, 0, 0, 0) format.'
+    raise ArgumentError, msg unless start_date.instance_of?(Time)
     start_date + 10**9
   end
 end
